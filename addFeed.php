189187d13a56b3d11c1fa93eff97ab76b57716ca
LazyClutch <lr_5146@163.com>
2015-12-15 18:11:29 +0800

    Update addFeed.php

--- addFeed.php
+++ addFeed.php
@@ -4,7 +4,7 @@ include 'requestFeed.php';
 $addr = $_GET["link"];
 $title = $_GET["name"];
 $addr = iconv("UTF-8", "ASCII", $addr);
-$mysql_con = mysql_connect("127.0.0.1:3306", "root", "aaawninhgyg");
+$mysql_con = mysql_connect("127.0.0.1:3306", "root", "123");
 if (!$mysql_con) {
     die('Could not connect: ' . mysql_error());
 }
